8767ab211772fc22ab9212fb6ecd1506ad101a9a
Michael Mustapha <mustaphamichael75@gmail.com>
2020-12-01 15:34:20 +0100

    Add swagger documentation

--- src/main/scala/app/routes/AuthorRoutes.scala
+++ src/main/scala/app/routes/AuthorRoutes.scala
@@ -6,6 +6,13 @@ import akka.http.scaladsl.server.Route
 import app.JsonSupport
 import app.data.persistence.DBSchema
 import app.data.{Author, Authors, ResponseBody}
+import io.swagger.v3.oas.annotations._
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.{Content, Schema}
+import io.swagger.v3.oas.annotations.parameters.RequestBody
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.{Consumes, DELETE, GET, POST, PUT, Path, Produces}
 
 import scala.concurrent.ExecutionContext
 
@@ -14,44 +21,104 @@ import scala.concurrent.ExecutionContext
  * @project - simple-crud
  * @author  - Michael Mustapha
  */
+@Path("/authors")
 class AuthorRoutes(repo: DBSchema#AuthorRepo)(implicit ec: ExecutionContext) extends JsonSupport {
 
-  val routes: Route = authors ~ author
-
+  val routes: Route =
   // routes with `/authors`
-  def authors: Route = path("authors") {
-    post {
-      entity(as[Author]) { author =>
-        onSuccess(repo.insert(author)) { d =>
-          complete(StatusCodes.Created, ResponseBody("Author created successfully", Some(d)))
-        }
+    path("authors") {
+      create ~ getAll ~ edit
+    } ~
+      // routes with `authors/{id}`
+      path("authors" / IntNumber) { id =>
+        getOne(id) ~ deleteOne(id)
       }
-    } ~ get {
-      onSuccess(repo.all) { d =>
-        complete(ResponseBody("Get authors successful", Some(Authors(d))))
-      }
-    } ~ put {
-      entity(as[Author]) { author =>
-        onSuccess(repo.update(author)) {
-          case data@Some(_) => complete(ResponseBody("A new author was created", data)) // Insert
-          case None => complete(ResponseBody("Request to update author was successful")) // Update
-        }
+
+  @POST
+  @Consumes(Array(MediaType.APPLICATION_JSON))
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  @Operation(summary = "Create an author", description = "Create an author", tags = Array("Author"),
+    requestBody = new RequestBody(content = Array(new Content(schema = new Schema(implementation = classOf[Author])))),
+    responses = Array(
+      new ApiResponse(responseCode = "200", description = "Author created successfully",
+        content = Array(new Content(schema = new Schema(implementation = classOf[ResponseBody])))))
+  )
+  def create: Route = post {
+    entity(as[Author]) { author =>
+      onSuccess(repo.insert(author)) { d =>
+        complete(StatusCodes.Created, ResponseBody("Author created successfully", Some(d)))
       }
     }
   }
 
-  // routes with `authors/{id}`
-  def author: Route = path("authors" / IntNumber) { id =>
-    get {
-      onSuccess(repo.findById(id)) {
-        case data@Some(_) => complete(ResponseBody("Get author successful", data))
-        case None => complete(StatusCodes.NotFound, ResponseBody("The author does not exist"))
+
+  @GET
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  @Operation(summary = "Get all authors", description = "Get all authors", tags = Array("Author"),
+    responses = Array(
+      new ApiResponse(responseCode = "200", description = "Get authors successful",
+        content = Array(new Content(schema = new Schema(implementation = classOf[Authors])))))
+  )
+  def getAll: Route = get {
+    onSuccess(repo.all) { d =>
+      complete(ResponseBody("Get authors successful", Some(Authors(d))))
+    }
+  }
+
+
+  @PUT
+  @Consumes(Array(MediaType.APPLICATION_JSON))
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  @Operation(summary = "Edit an author", description = "Edit an author", tags = Array("Author"),
+    requestBody = new RequestBody(content = Array(new Content(schema = new Schema(implementation = classOf[Author])))),
+    responses = Array(
+      new ApiResponse(responseCode = "200", description = "A new author will be created if the id does not exist",
+        content = Array(new Content(schema = new Schema(implementation = classOf[Author])),
+          new Content(schema = new Schema(implementation = classOf[ResponseBody])))))
+  )
+  def edit: Route = put {
+    entity(as[Author]) { author =>
+      onSuccess(repo.update(author)) {
+        case data@Some(_) => complete(ResponseBody("A new author was created", data)) // Insert
+        case None => complete(ResponseBody("Request to update author was successful")) // Update
       }
-    } ~ delete {
-      onSuccess(repo.delete(id)) { d =>
-        if (d) complete(ResponseBody("Delete author successful"))
-        else complete(StatusCodes.NotFound, ResponseBody("The author does not exist"))
-      }
+    }
+  }
+
+
+  @GET
+  @Path("/{id}")
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  @Operation(summary = "Get a single author", description = "Get a single author", tags = Array("Author"),
+    parameters = Array(new Parameter(name = "id", in = ParameterIn.PATH, required = true, description = "Book id")),
+    responses = Array(
+      new ApiResponse(responseCode = "200", description = "Get author successful",
+        content = Array(new Content(schema = new Schema(implementation = classOf[Author])))),
+      new ApiResponse(responseCode = "404", description = "Not Found",
+        content = Array(new Content(schema = new Schema(implementation = classOf[Author])))))
+  )
+  def getOne(@Parameter(hidden = true) id: Int): Route = get {
+    onSuccess(repo.findById(id)) {
+      case data@Some(_) => complete(ResponseBody("Get author successful", data))
+      case None => complete(StatusCodes.NotFound, ResponseBody("The author does not exist"))
+    }
+  }
+
+
+  @DELETE
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  @Path("/{id}")
+  @Operation(summary = "Delete a single author", description = "Delete a single author", tags = Array("Author"),
+    parameters = Array(new Parameter(name = "id", in = ParameterIn.PATH, required = true, description = "Author id")),
+    responses = Array(
+      new ApiResponse(responseCode = "200", description = "Author deleted",
+        content = Array(new Content(schema = new Schema(implementation = classOf[Author])))),
+      new ApiResponse(responseCode = "404", description = "Not Found"))
+  )
+  def deleteOne(@Parameter(hidden = true) id: Int): Route = delete {
+    onSuccess(repo.delete(id)) { d =>
+      if (d) complete(ResponseBody("Author deleted"))
+      else complete(StatusCodes.NotFound, ResponseBody("The author does not exist"))
     }
   }
 }
--- src/main/scala/app/swagger/SwaggerDocService.scala
+++ src/main/scala/app/swagger/SwaggerDocService.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.server.{Directives, Route}
 import app.AppConfig
 import app.routes.{AuthorRoutes, BookRoutes}
 import com.github.swagger.akka.SwaggerHttpService
-import com.github.swagger.akka.model.Info
+import com.github.swagger.akka.model.{Contact, Info}
 
 /*
  * @created - 01/12/2020
@@ -12,9 +12,13 @@ import com.github.swagger.akka.model.Info
  * @author  - Michael Mustapha
  */
 private object SwaggerDocService extends SwaggerHttpService with AppConfig {
-  override val apiClasses: Set[Class[_]] = Set(classOf[BookRoutes])
+  override val apiClasses: Set[Class[_]] = Set(classOf[BookRoutes], classOf[AuthorRoutes])
   override val host = s"$interface:$port"
-  override val info: Info = Info(version = "1.0")
+  override val info: Info = Info(
+    description = "A simple CRUD app using Akka-Http and PostgreSQL",
+    version = "1.0",
+    contact = Some(Contact(name = "Michael Mustapha", url = "https://github.com/mustaphamichael", email = "")))
+  override val unwantedDefinitions = Seq("Function1", "Function1RequestContextFutureRouteResult")
 }
 
 object SwaggerRoutes extends Directives {
